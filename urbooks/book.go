62f058b64d62019b7c1404783bb046e6a2b97fbb
ohzqq <iamchurkey@gmail.com>
2022-07-13 15:11:30 -0400

    clean up

--- urbooks/book.go
+++ urbooks/book.go
@@ -3,6 +3,7 @@ package urbooks
 import (
 	"fmt"
 	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/ohzqq/urbooks-core/calibredb"
@@ -170,25 +171,6 @@ func (b Book) GetFile(f string) *Item {
 	return bfile
 }
 
-type MetaString string
-
-func NewMetaString() *MetaString {
-	ms := MetaString("")
-	return &ms
-}
-
-func (ms *MetaString) SetValue(v string) *MetaString {
-	s := MetaString(v)
-	ms = &s
-	return ms
-}
-
-func (ms MetaString) URL() string                 { return "" }
-func (ms MetaString) IsNull() bool                { return ms == "" }
-func (ms MetaString) Value() string               { return string(ms) }
-func (ms MetaString) String() string              { return string(ms) }
-func (ms MetaString) FieldMeta() *calibredb.Field { return &calibredb.Field{} }
-
 func AudioFormats() []string {
 	return []string{"m4b", "m4a", "mp3", "opus", "ogg"}
 }
@@ -240,3 +222,172 @@ func BookCatsTitle(idx int) string {
 	}
 	return titles[idx]
 }
+
+type Category struct {
+	Field *calibredb.Field
+	query url.Values
+	items []*Item
+	value string
+	item  Item
+}
+
+const (
+	nameSep    = " & "
+	itemSep    = ", "
+	cliItemSep = `,`
+	cliNameSep = `&`
+)
+
+func NewCategory() *Category {
+	cat := &Category{}
+	return cat
+}
+
+func (c *Category) AddItem() *Item {
+	item := NewCategoryItem()
+	c.items = append(c.items, item)
+	return item
+}
+
+func (c Category) String() string {
+	return c.Join("value")
+}
+
+func (c Category) Join(v string) string {
+	var meta []string
+	for _, field := range c.items {
+		meta = append(meta, field.Get(v))
+	}
+	switch c.Field.IsNames {
+	case true:
+		return strings.Join(meta, nameSep)
+	default:
+		return strings.Join(meta, itemSep)
+	}
+}
+
+func (c Category) Cli(v string) string {
+	var meta []string
+	for _, field := range c.items {
+		meta = append(meta, field.Get(v))
+	}
+	switch c.Field.IsNames {
+	case true:
+		return strings.Join(meta, cliNameSep)
+	default:
+		return strings.Join(meta, cliItemSep)
+	}
+}
+
+func (c *Category) Split(value string, names bool) {
+	sep := itemSep
+	if names {
+		sep = nameSep
+	}
+	for _, val := range strings.Split(value, sep) {
+		c.AddItem().SetValue(val)
+	}
+}
+
+func (c Category) ItemStringSlice() []string {
+	var i []string
+	for _, item := range c.Items() {
+		i = append(i, item.String())
+	}
+	return i
+}
+
+func (c *Category) SetFieldMeta(k, v string) *Category {
+	switch k {
+	case "isNames":
+		if v == "true" {
+			c.Field.IsNames = true
+		}
+	}
+	return c
+}
+
+func (c Category) IsNull() bool                { return len(c.Items()) == 0 }
+func (c Category) FieldMeta() *calibredb.Field { return c.Field }
+func (c Category) Value() string               { return c.Join("value") }
+func (c Category) Items() []*Item              { return c.items }
+func (c Category) URL() string                 { return c.Field.Label + "/" }
+
+type Item struct {
+	Field *calibredb.Field
+	query url.Values
+	meta  map[string]string
+}
+
+func NewCategoryItem() *Item {
+	return &Item{meta: make(map[string]string)}
+}
+
+func (i Item) TotalBooks() int {
+	if t := i.Get("books"); t != "" {
+		b, err := strconv.Atoi(t)
+		if err != nil {
+			return 0
+		}
+		return b
+	}
+	return 0
+}
+
+func (i *Item) Set(k, v string) *Item {
+	i.meta[k] = v
+	return i
+}
+
+func (i *Item) SetValue(v string) *Item {
+	i.meta["value"] = v
+	return i
+}
+
+func (i Item) Value() string               { return i.Get("value") }
+func (i Item) String() string              { return i.Get("value") }
+func (i Item) FieldMeta() *calibredb.Field { return i.Field }
+func (i Item) IsNull() bool                { return len(i.meta) == 0 }
+func (i Item) ID() string                  { return i.Get("id") }
+func (i Item) URL() string                 { return i.Get("url") }
+func (i Item) Get(v string) string         { return i.meta[v] }
+
+type MetaString string
+
+func NewMetaString() *MetaString {
+	ms := MetaString("")
+	return &ms
+}
+
+func (ms *MetaString) SetValue(v string) *MetaString {
+	s := MetaString(v)
+	ms = &s
+	return ms
+}
+
+func (ms MetaString) URL() string                 { return "" }
+func (ms MetaString) IsNull() bool                { return ms == "" }
+func (ms MetaString) Value() string               { return string(ms) }
+func (ms MetaString) String() string              { return string(ms) }
+func (ms MetaString) FieldMeta() *calibredb.Field { return &calibredb.Field{} }
+
+type Column struct {
+	Field *calibredb.Field
+	query url.Values
+	meta  string
+}
+
+func NewColumn() *Column {
+	return &Column{}
+}
+
+func (c *Column) SetValue(v string) *Column {
+	c.meta = v
+	return c
+}
+
+func (c Column) URL() string                 { return "" }
+func (c Column) Value() string               { return c.meta }
+func (c Column) String() string              { return c.meta }
+func (c Column) FieldMeta() *calibredb.Field { return c.Field }
+func (c Column) IsNull() bool                { return c.meta == "" }
