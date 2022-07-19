50b483e38ebf056608ebd08697e2f6f67a3cb411
ohzqq <iamchurkey@gmail.com>
2022-07-19 15:40:06 -0400

    rename

--- calibredb/sql/column.sql
+++ calibredb/sql/column.sql
@@ -1,25 +1,54 @@
-{{define "column"}}
-{{- $f := GetJsonField .Label -}}
-{{- if ne $f "id"}}
-{{- if ne $f "url"}}
-{{- if ne $f "cover" -}}
+{{define "BookCol"}}
 
-IFNULL(JSON_QUOTE(
+{{- $lib := . -}}
 
-{{- if eq $f "modified" "added" "published" -}}
-	strftime('%Y-%m-%d', {{- .Label -}})
-{{- else if eq $f "uri" -}}
-	"books/" || id
-{{- else if eq $f "position" -}}
-	lower({{.Label}})
-{{- else -}}
-	{{- .Label -}}
-{{- end -}}
+IFNULL(JSON_QUOTE(lower(id)), '""') id,
+IFNULL(JSON_QUOTE(lower(series_index)), '""') position,
+{{range $field := .Fields.BookCol -}}
+	IFNULL(JSON_QUOTE({{$field}}), '""') {{GetJsonField $field}},
+{{end -}}
 
-), '""') {{$f}},
+{{- range $field := .Fields.DateCol -}}
+	IFNULL(JSON_QUOTE(strftime('%Y-%m-%d', {{$field}})), '""') {{GetJsonField $field}},
+{{end -}}
 
-{{- end -}}
-{{- end -}}
-{{- end -}}
+IFNULL(
+CASE has_cover
+WHEN true
+THEN JSON_OBJECT(
+	'basename', 'cover',
+	'extension', 'jpg',
+	'path', "{{$lib.Path}}" || "/" || path || "/cover.jpg",
+	'uri', "books/" || books.id || "/cover.jpg",
+	'value', 'cover.jpg'
+)
+END, '{}') cover, 
+
+JSON_QUOTE(
+title || IFNULL(" [" || (
+	SELECT name 
+	FROM series 
+	WHERE series.id 
+	IN (
+		SELECT series 
+		FROM books_series_link 
+		WHERE book=books.id
+	)
+) || ", Book " || (series_index) || "]", "")) titleAndSeries, 
+
+IFNULL(JSON_QUOTE((
+SELECT text 
+FROM comments 
+WHERE book=books.id)), '""') description,
+
+IFNULL((
+SELECT JSON_QUOTE(lower(rating))
+FROM ratings 
+WHERE ratings.id 
+IN (
+	SELECT rating 
+	FROM books_ratings_link 
+	WHERE book=books.id
+)), '""') rating,
+
 {{end}}
-
